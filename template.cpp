bc6ded18e6bfa93f94690aa9cad9a1c9ae779757
tmpdeatheye <tmpdeatheye@gmail.com>
2024-07-24 01:26:02 +0700

    [algorithm] init

---
+++ template.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef int32_t i32;
+
+i32 t;
+
+void solve() {
+
+}
+
+i32 main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cin >> t;
+    // t = 1;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
